519efdd18375a081f6a76b7b49492e16936e2a81
yanruijie902136 <yanruijie902136@gmail.com>
2025-03-08 11:05:45 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -16,14 +16,10 @@ def main():
     with open(filename) as file:
         file_contents = file.read()
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-    # Uncomment this block to pass the first stage
-    # if file_contents:
-    #     raise NotImplementedError("Scanner not implemented")
-    # else:
-    #     print("EOF  null") # Placeholder, remove this line when implementing the scanner
+    if file_contents:
+        raise NotImplementedError("Scanner not implemented")
+    else:
+        print("EOF  null")
 
 
 if __name__ == "__main__":
